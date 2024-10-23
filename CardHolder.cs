c1dc82a35bfe51fcc500178a8ba2b503808e07df
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-10-23 15:03:07 +0100

    Added firstname and lastname properties and encapsulated the internal private data members

--- CardHolder.cs
+++ CardHolder.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ATM_Emulator
 {
@@ -10,5 +10,41 @@ namespace ATM_Emulator
         private string _firstName;
         private string _pin;
         private string _lastName;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if(isValidName(_firstName) && _firstName.Length > 0)
+                {
+                    _firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Provide a last name and make sure there are no special characters.");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if(isValidName(_lastName) && _lastName.Length > 0)
+                {
+                    _lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Provide a last name and make sure there are no special characters.");
+                }
+            }
+        }
+
+
+
+        private bool isValidName(string name) => Regex.IsMatch(name, @"^[a-zA-Z\s]+$");
     }
 }
